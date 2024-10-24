375a332ebd63035e60dcdabf7410263f25f50698
Muhammad Sharjeel Asif <m.sharjeelasif1435@gmail.com>
2024-10-24 22:36:36 +0500

    error resolved

--- Blog-Website-Figma/app/Components/Footer.tsx
+++ Blog-Website-Figma/app/Components/Footer.tsx
@@ -11,7 +11,7 @@ export default function Footer() {
       <div className="flex justify-center gap-10">
         <Link href='https://www.facebook.com/profile.php?id=100085035428339' target="blank"><Image src={Facebook} height={30} width={30} alt="fb" /></Link>
         <Link href='https://www.instagram.com/m_sharjeel_1435/'><Image src={Insta} height={30} width={30} alt="fb" /></Link>
-        <Link href='https://www.facebook.com/profile.php?id=100085035428339'><Image src={Twitter} height={30} width={30} alt="fb" /></Link>
+        <Link href='https://github.com/Muhammad-Sharjeel-Asif'><Image src={Twitter} height={30} width={30} alt="fb" /></Link>
         <Link href='https://www.linkedin.com/in/muhammad-sharjeel-asif-6a08532b9/'><Image src={LinkedIn} height={30} width={30} alt="fb" /></Link>
       </div>
       <p className="text-black text-center text-xl font-semibold py-8">Copyright &copy;2020 All rights reserved</p>
